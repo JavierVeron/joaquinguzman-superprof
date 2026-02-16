b284623c0bcfcd3c4c8a6da5582abba9535aea2c
Javier Veron <javier.veron@gmail.com>
2026-02-16 11:12:06 -0300

    Clase 1

---
+++ clase1/script.js
@@ -0,0 +1,20 @@
+//document.getElementById("nombre").value = "Joaquin"
+
+/* const miForm = document.getElementById("miForm");
+
+for (let i=1; i<=20; i++) {
+    const miDiv = document.createElement("div");
+    miDiv.className = "campos";
+    const miEtiqueta = document.createElement("label");
+    miEtiqueta.innerHTML = "Alumno #" + i;
+    const nuevoCampo = document.createElement("input");
+    nuevoCampo.value = "Joaquin Guzman"
+    miDiv.appendChild(miEtiqueta);
+    miDiv.appendChild(nuevoCampo);
+    miForm.appendChild(miDiv);
+} */
+
+// Promesas => Es similar (devuelve un valor y un estado) => tiene 3 estados
+// Pendiente (pending)
+// Cumplida (fullfilled)
+// Rechazada (rejected)
